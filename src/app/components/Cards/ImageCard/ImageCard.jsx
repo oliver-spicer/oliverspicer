bc3b2ae3d4965cbd888553d20fc34fd2f8cbc607
Oli Spicer <oli.spicer@gmail.com>
2023-11-06 22:51:46 +0100

    Continued applying bootstrap styles to the main page

--- src/app/components/Cards/ImageCard/ImageCard.jsx
+++ src/app/components/Cards/ImageCard/ImageCard.jsx
@@ -1,5 +1,19 @@
+import Image from 'next/image';
 import styles from './ImageCard.module.scss';
 
-export default function ImageCard({ children }) {
-  return <div>{children}</div>;
+export default function ImageCard({ image, ratio, children }) {
+  const imageRatio = ratio ?? '1-1';
+
+  return (
+    <div className={`col`}>
+      <div
+        className={`${styles.imageCardWrapper} ${styles['ratio' + imageRatio]}`}
+      >
+        {image && <Image src={image} fill={true} alt="image" />}
+        <div className={`text-center fs-1 ${styles.textWrapper}`}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,15 @@ import Button from './components/Button';
 import ImageCard from './components/Cards/ImageCard';
 import Heading from './components/Heading';
 
+import {
+  greenLeafedTrees,
+  snowyMountain,
+  topOfMountain,
+  projectIci,
+  projectLs,
+  projectRu,
+} from '@/images';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './globals.scss';
@@ -13,8 +22,8 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <section className="container-sm text-center py-5">
-        <p className={`${styles.mainHeader} h1`}>Hi. I'm Oliver</p>
+      <section className="container-xl text-center py-5 my-5">
+        <p className={`${styles.fsHeader1} h1`}>Hi. I'm Oliver</p>
         <Heading tag="h1" appearence="h2">
           A Passionate software engineer with an <br />
           obession with web design & development.
@@ -24,50 +33,68 @@ export default function Home() {
           <br />
           on intuitive user experiences.
         </Heading>
-        <div>
-          <Button variant="outline-primary">Services</Button>
-          <Button variant="outline-primary">My Story</Button>
+        <div className="pt-3">
+          <Button variant="primary">Services</Button>
+          <Button variant="primary">My Story</Button>
         </div>
       </section>
-      <section>
-        <div>
-          <ImageCard>Design</ImageCard>
-          <ImageCard>Development</ImageCard>
-          <ImageCard>Integration</ImageCard>
+      <section className="container-xl py-5 my-5">
+        <div className="row mx-5">
+          <ImageCard image={greenLeafedTrees}>Design</ImageCard>
+          <ImageCard image={snowyMountain}>Development</ImageCard>
+          <ImageCard image={topOfMountain}>Integration</ImageCard>
         </div>
       </section>
-      <section className="container-xl">
-        <div>
-          <Heading tag="h3">A little about me</Heading>
-        </div>
-        <div>
-          <p>
-            Over the past 10+ years, I’ve worked on various projects from web
-            design to software architecture. All the while, I’ve continued to
-            grow as an IT expert and as a person.
-          </p>
-          <p>
-            These days, I’m focused on bringing great web experiences to life
-            through my passion for coding.
-          </p>
-          <p>If you’d like to read more, check out my story.</p>
-          <Button>My Story</Button>
+      <section className="container-xl py-5 my-5">
+        <div className="row mx-5">
+          <div className="col">
+            <Heading tag="h3" appearence="h2">
+              A little about me
+            </Heading>
+          </div>
+          <div className="col">
+            <p>
+              Over the past 10+ years, I’ve worked on various projects from web
+              design to software architecture. All the while, I’ve continued to
+              grow as an IT expert and as a person.
+            </p>
+            <p>
+              These days, I’m focused on bringing great web experiences to life
+              through my passion for coding.
+            </p>
+            <p>If you’d like to read more, check out my story.</p>
+            <Button>My Story</Button>
+          </div>
         </div>
       </section>
-      <section className="container-xl">
-        <Heading tag="h2">You're still here ?</Heading>
-        <p>
+      <section
+        className="container-fluid my-5 text-center"
+        style={{
+          backgroundImage: `url(/images/log-pile.jpg)`,
+          backgroundSize: 'cover',
+          padding: '120px 0',
+          color: '#fff',
+        }}
+      >
+        <Heading tag="h2" appearence="h1">
+          You're still here ?
+        </Heading>
+        <p className="py-3">
           If you’re reading this, I’ve successfully caught your attention. Great
-          ! I’m always open to discussing potential collaborations.
+          !
+          <br />
+          I’m always open to discussing potential collaborations.
         </p>
-        <Button>Let's work together</Button>
+        <Button variant="outline-success">Let's work together</Button>
       </section>
-      <section className="container-xl">
-        <Heading tag="h2">Projects</Heading>
-        <div>
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
+      <section className="container-xl py-5 my-5 text-center">
+        <Heading tag="h2" appearence="h1">
+          Projects
+        </Heading>
+        <div className="row py-5">
+          <ImageCard image={projectIci} ratio="16-9" />
+          <ImageCard image={projectLs} ratio="16-9" />
+          <ImageCard image={projectRu} ratio="16-9" />
         </div>
       </section>
       <Footer />
